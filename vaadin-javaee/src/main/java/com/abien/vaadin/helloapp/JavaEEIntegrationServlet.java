ab2f7608a31f1c9684836fa1a21fcbb26e15c11e
Adam Bien <abien@adam-bien.com>
2012-08-07 05:54:41 +0200

    Apache License added

--- vaadin-javaee/src/main/java/com/abien/vaadin/helloapp/JavaEEIntegrationServlet.java
+++ vaadin-javaee/src/main/java/com/abien/vaadin/helloapp/JavaEEIntegrationServlet.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2012 Adam Bien, adam-bien.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.abien.vaadin.helloapp;
 
 import com.vaadin.Application;
@@ -7,6 +22,10 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ *
+ * @author adam-bien.com
+ */
 @WebServlet(urlPatterns = "/*")
 public class JavaEEIntegrationServlet extends AbstractApplicationServlet {
 
